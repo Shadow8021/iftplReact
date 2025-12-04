085e6656d677aa11485a852aa63385ed03facc8f
Shadow0821 <gregoyaga@gmail.com>
2025-12-04 21:58:39 +0100

    fin de la premiere partie

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Contenaire from './components/Contenaire'
 import Navbar from './components/Navbar'
 import Heros from './components/hero/Heros'
 import Programme from './components/progammes/Programme'
+import Accesiftpl from './components/accesiftpl/Accesiftpl'
 export default function App() {
   return (
     <div>
       <Navbar />
       <Heros />
       <Programme />
+      <Accesiftpl />
     </div>
   )
 }
---
+++ src/components/accesiftpl/Accesiftpl.jsx
@@ -0,0 +1,19 @@
+import image from "../../../public/iftpl.jpg"
+export default function Accesiftpl() {
+    return (
+        <section className="w-full flex flex-col items-center justify-evenly py-10 px-5 h-auto contenaire lg:flex-row gap-9">
+            <div class="w-full">
+                <img src={image} alt="toto" />
+            </div>
+            <div className="w-full">
+                <h3 className="text-[2.5rem] py-5 font-bold">Commencez votre
+                    parcours de baccalaureat à I.F.T.P.L</h3>
+                <div className="shadow-2xl p-4 bg-[#dbdbda] rounded-lg">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati tempore quis deleniti saepe possimus
+                        molestiae soluta id impedit incidunt ipsa, praesentium alias rem, sint et sed, veniam quibusdam nam dolorem
+                        est. Asperiores nihil neque ex facere ab mollitia consectetur similique.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
