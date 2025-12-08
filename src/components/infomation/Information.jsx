1565777afd1c9b40df1485dcbbd07174d7ccb581
martial Oyaga <gregoyaga@gmail.com>
2025-12-08 19:22:28 +0100

    fin de la section info

--- src/components/infomation/Information.jsx
+++ src/components/infomation/Information.jsx
@@ -5,7 +5,15 @@ export default function Information() {
     <div className="w-full flex flex-col gap-4 px-5 py-2">
       {
         donnees.map(dta=>(
-            <p>lorem ert</p>
+             <div
+        className="flex w-full text-[#D00D2D] justify-center gap-2 bg-[#002E6D] border border-blue-600 shadow-2xl rounded-2xl">
+        <div className="w-90 h-30 p-3 flex items-center rounded-full lg:h-30 lg:w-40">
+          <img className="w-full" src={dta.logo} alt="logo"/>
+        </div>
+        <div class=" w-200 h-30 flex items-center py-5 p-3 ">
+          <p class="text-[12px] font-bold text-white lg:text-[15px]">{dta.texte}</p>
+        </div>
+      </div>
             
         ))
       }
--- src/components/infomation/data.js
+++ src/components/infomation/data.js
@@ -5,12 +5,12 @@ export let donnees = [
         texte: "Des formations en pluridisciplinaires qui permettent à l'étudiant de lier harmonieusement théorie et pratique, tout en cultivant son savoir-faire et son savoir-être ."
     }, {
         id: 2,
-        logo: "../../../public/icones/apartement.svg",
+        logo: "../../../public/icones/apartment.svg",
         texte: "Des résidences sécurisées et entierement équipées, offrant un logement agréable et personnalisé. "
     }, {
         id: 3,
         logo: "../../../public/icones/lab_panel.svg",
-        titre: "Des laboratoires et ateliers dotés d'équipements de pointe, permettant de simuler avec précision les réalités industrielles et d'offrir une immersion complète pour tous."
+        texte: "Des laboratoires et ateliers dotés d'équipements de pointe, permettant de simuler avec précision les réalités industrielles et d'offrir une immersion complète pour tous."
     }
 ]
 
