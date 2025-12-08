436cbe80b84a7cb5482d4dc424de7b06f595b598
martial Oyaga <gregoyaga@gmail.com>
2025-12-08 20:08:02 +0100

    configuration des stats

---
+++ src/components/statistique/stats.js
@@ -0,0 +1,28 @@
+export let donnees = [
+    {
+        id: 1,
+        nbr: "+7",
+        texte: "Formations"
+    }, {
+        id: 1,
+        nbr: "2",
+        texte: "langages"
+    }, {
+        id: 1,
+        nbr: "+200",
+        texte: "étudiants"
+    }, {
+        id: 1,
+        nbr: "+500",
+        texte: "lauréats"
+    }, {
+        id: 1,
+        nbr: "30",
+        texte: "Personnel Administratif"
+    }, {
+        id: 1,
+        nbr: "+51",
+        texte: "Personnel enseignants"
+    }
+]
+
