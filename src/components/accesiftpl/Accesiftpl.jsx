f5378826aebb24874406d6c0750daa83940a8a9f
Shadow8021 <gregoyaga@gmail.com>
2026-01-15 11:23:09 +0100

    fin de la page

--- src/components/accesiftpl/Accesiftpl.jsx
+++ src/components/accesiftpl/Accesiftpl.jsx
@@ -3,10 +3,10 @@ export default function Accesiftpl() {
     return (
         <section className="w-full flex flex-col items-center justify-evenly py-10 px-5 h-auto contenaire lg:flex-row gap-9">
             <div className="w-full">
-                <img src={image} alt="toto" />
+                <img className=" rounded-2xl" src={image} alt="toto" />
             </div>
             <div className="w-full">
-                <h3 className="text-[2.5rem] py-5 font-bold">Commencez votre
+                <h3 className="text-[2.5rem] py-5 font-bold text-white">Commencez votre
                     parcours de baccalaureat à I.F.T.P.L</h3>
                 <div className="shadow-2xl p-4 bg-[#dbdbda] rounded-lg">
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati tempore quis deleniti saepe possimus
--- src/components/projectForm/ProjectForm.jsx
+++ src/components/projectForm/ProjectForm.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function ProjectForm() {
   return (
-        <div className="w-full flex flex-col h-auto px-5 py-5 gap-1">
+    <div className="w-full flex flex-col h-auto px-5 py-5 gap-1">
       <div className="w-full mx-auto py-5 lg:flex lg:justify-evenly lg:items-center lg:gap-3">
         <h3 className="text-3xl block pl-5 font-bold text-[#D00D2D]  lg:text-wrap lg:max-w-100 lg:text-[2.5rem]">Devenir un
           technicien
           qualifier avec nos
           formations</h3>
-        <p className="text-sm w-full text-justify pt-5">Nos programmes s’articulent autour de cours immersifs qui vous
+        <p className="text-xl max-w-xl w-full text-justify pt-5">Nos programmes s’articulent autour de cours immersifs qui vous
           permettent
           d’apprendre en
           pratiquant, en
