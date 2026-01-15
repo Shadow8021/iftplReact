2a88001a3a1fb463b429889ca85d7f18fd8f7e9d
Shadow8021 <gregoyaga@gmail.com>
2026-01-15 21:32:15 +0100

    configuration des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Heros from './components/hero/Heros'
-import Programme from './components/progammes/Programme'
-import Accesiftpl from './components/accesiftpl/Accesiftpl'
-import Information from './components/infomation/Information'
-import ProjectForm from './components/projectForm/ProjectForm'
-import Statistique from './components/statistique/Statistique'
-import Partenaires from './components/partenaire/Partenaires'
-import Temoignages from './components/Temoignages/Temoignages'
-import { Contacts } from './components/contacts/Contacts'
-import Footer from './components/footer/Footer'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact"
@@ -25,18 +14,6 @@ export default function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
-
-      <Navbar />
-      <Heros />
-      <Programme />
-      <Accesiftpl />
-      <Information />
-      <ProjectForm />
-      <Statistique />
-      <Partenaires />
-      <Temoignages />
-      <Contacts />
-      <Footer />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import menu from "../../public/icones/mymenu.svg"
 import logo from "../../public/logo1.svg"
+import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <div className="sticky top-0 left-0 z-20">
             <nav className='flex w-full bg-[#002E6D] text-white h-18 items-center justify-between px-3 lg:px-20 '>
                 <img className="h-15" src={logo} alt="logo" />
                 <ul className='gap-6 hidden font-bold lg:flex sm:flex'>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">Home</a></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">A propos</a></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">Formations
-                    </a></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">Actualités</a></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">Galerie
-                    </a></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><a href="#">Contact</a></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link to="/">Home</Link></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link to="/about">A propos</Link></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link to="/formation">Formations
+                    </Link></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link to="/actualite">Actualités</Link></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link to="/galerie">Galerie
+                    </Link></li>
+                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><Link href="/contact">Contact</Link></li>
 
                 </ul>
                 <img src={menu} className="lg:hidden sm:hidden" alt="menu" />
