6ab8730010f0f92b35a70d24c3ff8247924e6c07
Shadow8021 <gregoyaga@gmail.com>
2026-01-15 18:25:16 +0100

    initialisation des pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,22 @@ import Partenaires from './components/partenaire/Partenaires'
 import Temoignages from './components/Temoignages/Temoignages'
 import { Contacts } from './components/contacts/Contacts'
 import Footer from './components/footer/Footer'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+
 export default function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+
       <Navbar />
       <Heros />
       <Programme />
