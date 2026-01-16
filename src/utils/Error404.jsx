d104a80c34c681b0229123c04305972698b3e7ef
Shadow8021 <gregoyaga@gmail.com>
2026-01-16 20:15:31 +0100

    fin de la configuration de la page 404

--- src/utils/Error404.jsx
+++ src/utils/Error404.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import img404 from "../../public/Img404.png"
 export default function Error404() {
     return (
-        <div>Error404</div>
+        <div className='w-ful m-3 bg-teal-50 shadow-2xl rounded-2xl h-125 p-5'>
+
+            <div className='w-ful m-3 flex justify-evenly items-center bg-teal-50 rounded-2xl h-90 p-5'>
+                <div className="flex items-center flex-col">
+                    <p className='text-8xl font-bold text-red-600'>404</p>
+                    <p className='text-2xl font-bold'>Not found</p>
+                </div>
+                <div>
+                    <img src={img404} alt="404image" className="w-80 mb-3" />
+                </div>
+            </div>
+        </div>
     )
 }
