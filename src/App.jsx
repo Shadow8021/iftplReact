723096591489eef18c1a51e567b82966ddf72c6c
Shadow8021 <gregoyaga@gmail.com>
2026-01-16 16:49:41 +0100

    ajout de la page 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact"
 import Home from "./pages/Home/Home"
 import Galerie from "./pages/Galerie/Galerie"
 import Actualite from "./pages/Actualite/Actualite"
+import Error404 from './utils/Error404';
 
 export default function App() {
   return (
@@ -12,10 +13,12 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/galerie" element={<Galerie />} />
           <Route path="/actualite" element={<Actualite />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </div>
