71d12b63676a6abf2a934a04d60a9ecbdb17cf4b
shadow <gregoyaga@gmail.com>
2026-02-02 18:05:48 +0100

    amelioration de projetform

--- src/components/projectForm/ProjectForm.jsx
+++ src/components/projectForm/ProjectForm.jsx
@@ -3,15 +3,12 @@ import React from 'react'
 export default function ProjectForm() {
   return (
     <div className="w-full flex flex-col h-auto px-5 py-5 gap-1">
-      <div className="w-full mx-auto py-5 lg:flex lg:justify-evenly lg:items-center lg:gap-3">
-        <h3 className="text-3xl block pl-5 font-bold text-[#D00D2D]  lg:text-wrap lg:max-w-100 lg:text-[2.5rem]">Devenir un
-          technicien
-          qualifier avec nos
-          formations</h3>
-        <p className="text-xl max-w-xl w-full text-justify pt-5 font-bold">Nos programmes s’articulent autour de cours immersifs qui vous
-          permettent
-          d’apprendre en
-          pratiquant, en
+      <div className="w-full mx-auto py-5 flex flex-col md:flex-row md:justify-evenly md:items-center lg:gap-3">
+        <h3 className="text-lg  block pl-5 font-bold text-[#D00D2D] md:text-left md:text-5xl  lg:text-wrap md:max-w-90 lg:max-w-100 lg:text-[2.5rem]">Devenir un
+          technicien qualifier avec nos formations
+        </h3>
+        <p className="text-xl  max-w-xl w-full text-justify pt-5 font-bold md:text-lg">Nos programmes s’articulent autour de cours immersifs qui vous
+          permettent d’apprendre en pratiquant, en
           expérimentant et en vous trompant, individuellement ou en groupe.
           Cette pédagogie active et inductive, fondée sur le principe « apprendre à apprendre », fait de vous les
           acteurs centraux de votre formation. Elle favorise l’acquisition de soft skills et de savoir-faire essentiels,
