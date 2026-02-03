251e06b748f91d95aaec59213138bd344750caf1
Shadow <gregoyaga@gmail.com>
2026-02-03 12:35:19 -0500

    deplacement du loader

---
+++ src/pages/layouts/loading.jsx
@@ -0,0 +1,15 @@
+import './Ripple.css'
+export default function Loading() {
+  return (
+    <div className="flex min-h-full my-50 justify-center items-center">
+        <div className="loadingio-spinner-ripple-2by998twmg8">
+          <div className="ldio-yzaezf3dcmj">
+            <div></div><div></div>
+            <div></div>
+          </div>
+        </div>
+    </div>
+
+  )
+}
+
