d551767bfdd43c354e8dfa697c288c9ce0440d28
Shadow <gregoyaga@gmail.com>
2026-02-03 12:22:53 -0500

    modification de la nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,34 @@
-import menu from "../../public/icones/mymenu.svg"
-import logo from "../../public/logo1.svg"
+import menu from "../../public/icones/mymenu.svg";
+import logo from "../../public/logo1.svg";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-
+    const items = [
+        {
+        nom :"Home",
+        path:"/"
+    },{
+        nom:"A propos",
+        path:""
+    },{
+        nom:"Formations",
+        path:"/formation"
+    },{
+        nom:"Galerie",
+        path:"/galerie"
+    },{
+        nom:"Contact",
+        path:"/contact"
+    }
+]
     return (
         <div className="sticky top-0 left-0 z-20 max-w-full">
             <nav className='flex w-full bg-[#002E6D] text-white h-18 items-center justify-between px-3 lg:px-20 '>
                 <NavLink to="/"><img className="h-15" src={logo} alt="logo" /></NavLink>
                 <ul className='gap-6 flex flex-row font-bold md:flex'>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/">Home</NavLink></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/about">A propos</NavLink></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer" ><NavLink to="/formation">Formations
-                    </NavLink></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/actualite">Actualités</NavLink></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/galerie">Galerie
-                    </NavLink></li>
-                    <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/contact">Contact</NavLink></li>
-
+                    {items.map((el,index)=>(
+                        <li key={index} className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to={el.path}>{el.nom}</NavLink></li>
+                    ))}
                 </ul>
                 <img src={menu} className="lg:hidden sm:hidden" alt="menu" />
             </nav>
