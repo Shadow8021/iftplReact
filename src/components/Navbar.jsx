919796feb7bb13d2305a1e738775f78159b026dd
Shadow <gregoyaga@gmail.com>
2026-02-03 12:07:13 -0500

    rectification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
 
     return (
-        <div className="sticky top-0 left-0 z-20">
+        <div className="sticky top-0 left-0 z-20 max-w-full">
             <nav className='flex w-full bg-[#002E6D] text-white h-18 items-center justify-between px-3 lg:px-20 '>
                 <NavLink to="/"><img className="h-15" src={logo} alt="logo" /></NavLink>
-                <ul className='gap-6 flex flex-row font-bold lg:flex sm:flex'>
+                <ul className='gap-6 flex flex-row font-bold md:flex'>
                     <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/">Home</NavLink></li>
                     <li className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to="/about">A propos</NavLink></li>
                     <li className="hover:text-[#D00D2D] transition ease-in pointer" ><NavLink to="/formation">Formations
--- src/components/hero/Heros.jsx
+++ src/components/hero/Heros.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default function () {
     return (
-        <section className=" w-full h-auto max-w hero">
+        <section className=" w-full h-auto max-w-full hero">
             <div className="w-full h-100 heroFilter p-10 flex flex-col items-center justify-center lg:h-150">
                 <h3 className="text-bold text-3xl mx-auto wrap max-w-200 text-center mt-5"><span
                     className="text-[#D00D2D] font-bold">I.F.T.P.L</span>, le centre de reference où se forment les techniciens,
