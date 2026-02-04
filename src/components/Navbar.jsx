1715fc08ee63725113a00b0501dcd9d2aee36fc5
Shadow <gregoyaga@gmail.com>
2026-02-04 13:41:42 -0500

    changement de l'icone du menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import menu from "../../public/icones/mymenu.svg";
+
 import logo from "../../public/logo1.svg";
+import { Menu } from 'lucide-react';
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
@@ -25,13 +26,14 @@ export default function Navbar() {
         <div className="sticky top-0 left-0 z-20 max-w-full">
             <nav className='flex w-full bg-[#002E6D] text-white h-18 items-center justify-between px-3 lg:px-20 '>
                 <NavLink to="/"><img className="h-15" src={logo} alt="logo" /></NavLink>
-                <ul className='gap-6 flex flex-row font-bold md:flex'>
+                <ul className='gap-6 flex-row font-bold hidden md:flex '>
                     {items.map((el,index)=>(
                         <li key={index} className="hover:text-[#D00D2D] transition ease-in pointer"><NavLink to={el.path}>{el.nom}</NavLink></li>
                     ))}
                 </ul>
-                <img src={menu} className="lg:hidden sm:hidden" alt="menu" />
+                <Menu size={40} className="lg:hidden sm:hidden" strokeWidth={2.25} />
             </nav>
         </div>
     )
 }
+
