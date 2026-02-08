47b76fd72e1fe8a60231ea610e52ff56f0decd8c
Shadow <gregoyaga@gmail.com>
2026-02-08 06:31:40 -0500

    responsive projectform

--- src/components/projectForm/ProjectForm.jsx
+++ src/components/projectForm/ProjectForm.jsx
@@ -5,7 +5,7 @@ export default function ProjectForm() {
         <h3 className="text-lg  block pl-5 font-bold text-[#D00D2D] md:text-left md:text-5xl  lg:text-wrap md:max-w-90 lg:max-w-100 lg:text-[2.5rem]">Devenir un
           technicien qualifier avec nos formations
         </h3>
-        <p className="text-xl  max-w-xl w-full text-justify pt-5 font-bold md:text-lg">Nos programmes s’articulent autour de cours immersifs qui vous
+        <p className="text-sm md:text-xl  max-w-xl w-full text-justify pt-5 font-bold">Nos programmes s’articulent autour de cours immersifs qui vous
           permettent d’apprendre en pratiquant, en
           expérimentant et en vous trompant, individuellement ou en groupe.
           Cette pédagogie active et inductive, fondée sur le principe « apprendre à apprendre », fait de vous les
