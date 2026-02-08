865429d7d893378b13f5c286bf70d84b64e8ccdc
Shadow <gregoyaga@gmail.com>
2026-02-08 08:14:47 -0500

    structure de la galerie

--- src/pages/Galerie/Galerie.jsx
+++ src/pages/Galerie/Galerie.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Galerie() {
     return (
-        <div>Galerie</div>
+        <div className='w-full p-10 h-200 mx-autom mb-50'>
+            <div className="flex items-center max-w-100 mx-auto justify-evenly gap-2 px-20 bg-red-200 py-1">
+                <div className="rounded-lg p-2 bg-yellow-200">Tous</div>
+                <div className="rounded-lg p-2 bg-yellow-200">Images</div>
+                <div className="rounded-lg p-2 bg-yellow-200">Videos</div>
+            </div>
+            <div className='bg-green-400 w-full p-10 h-200 mx-auto flex gap-2 justify-evenly flex-wrap'>
+                <div className="w-40 bg-red-400 h-50"></div>
+                <div className="w-40 bg-red-400 h-50"></div>
+                <div className="w-40 bg-red-400 h-50"></div>
+                <div className="w-40 bg-red-400 h-50"></div>
+                <div className="w-40 bg-red-400 h-50"></div>
+            </div>
+            <div className="flex items-center max-w-100 mx-auto justify-evenly gap-2 px-20 bg-red-200 py-1">
+                <div className="rounded-lg p-2 bg-yellow-200">Tous</div>
+                <div className="rounded-lg p-2 bg-yellow-200">Images</div>
+                <div className="rounded-lg p-2 bg-yellow-200">Videos</div>
+            </div>
+        </div>
     )
 }
