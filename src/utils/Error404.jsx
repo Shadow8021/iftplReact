98ad23ba87867a6298f0e92ceb0da76c7c403f0f
Shadow821 <gregoyaga@gmail.com>
2026-02-13 13:37:34 +0100

    amelioration de la page 404

--- src/utils/Error404.jsx
+++ src/utils/Error404.jsx
@@ -1,9 +1,9 @@
 import img404 from "/Img404.png"
 export default function Error404() {
     return (
-        <div className='w-ful m-3 bg-teal-50 shadow-2xl rounded-2xl h-125 p-5'>
+        <div className='w-full m-0 h-screen bg-teal-50 shadow-2xl p-5'>
 
-            <div className='w-ful m-3 flex justify-evenly items-center bg-teal-50 rounded-2xl h-90 p-5'>
+            <div className='w-full flex flex-col md:flex-row justify-evenly items-center bg-teal-50 rounded-2xl h-screen p-5'>
                 <div className="flex items-center flex-col">
                     <p className='text-8xl font-bold text-red-600'>404</p>
                     <p className='text-2xl font-bold'>Not found</p>
