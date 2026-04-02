a11aefce1b7d344c2be84ba9a7223e42a8c3faab
Shadow8021 <gregoyaga@gmail.com>
2026-04-02 22:37:40 +0100

    hachage de mdp

--- frontend/src/admin/auth/loginAdmin.tsx
+++ frontend/src/admin/auth/loginAdmin.tsx
@@ -1,12 +1,36 @@
+import bcrypt from 'bcryptjs';
 type LoginAdminProps = {
     userName: string;
     password: string;
 };
-export function LoginAdmin({ userName, password }: LoginAdminProps): boolean {
-    if (userName === "admin@iftpl.com" && password === "admin") {
-        return true;
-    } else {
-        return false;
+type HashPasswordResult = {
+    hashedPassword: string;
+};
+
+const saltRounds = 10; // Facteur de travail
+
+const hashPassword = async (plainPassword: string): Promise<HashPasswordResult> => {
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(plainPassword, salt);
+        return { hashedPassword };
+    } catch (error) {
+        throw error;
     }
+};
+
+
+
+export function LoginAdmin({ userName, password }: LoginAdminProps): boolean {
+    hashPassword(password).then(() => {
+        if (userName === "admin@iftpl.com" && password === "admin") {
+            return true;
+        } else {
+            return false;
+        }
+    }).catch(error => {
+        console.error('Erreur dans LoginAdmin :', error);
+    });
+    return true;
 }
 
